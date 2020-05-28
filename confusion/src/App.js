33fdb4c43d66a2bc054b507d303e2e88fb81fd25
Vaibhav Garg <vaibhavgarg889@gmail.com>
2020-05-28 21:09:10 +0530

    exercise 2

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -3,8 +3,15 @@ import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import './App.css';
 import Menu from './Components/Menu'
+import {DISHES} from './shared/dishes'
 
 class App extends Component {
+  constructor(props){
+    super(props);
+      this.state = {
+        dishes: DISHES,
+      }
+  }
   render() {
     return (
       <div className="App">
@@ -14,7 +21,7 @@ class App extends Component {
       </div>    
 
       </Navbar>
-      <Menu />
+      <Menu dishes={this.state.dishes}/>
       </div> 
       
     );
